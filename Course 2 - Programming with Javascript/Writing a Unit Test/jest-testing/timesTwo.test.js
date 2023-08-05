ab0adf77585bf0ac6d8c806028d921c8064c1d59
kkw123 <kingwye.kum@my.jcu.edu.au>
2023-08-05 17:22:46 +0800

    Completed "Unit Test" Assignment

--- Course 2 - Programming with Javascript/Writing a Unit Test/jest-testing/timesTwo.test.js
+++ Course 2 - Programming with Javascript/Writing a Unit Test/jest-testing/timesTwo.test.js
@@ -1,6 +1,10 @@
+const { default: TestRunner } = require("jest-runner");
 const timesTwo = require("./timesTwo");
 
 // Write the first test
-test("returns ...", () => {
-	expect().toBe();
+test("returns the number times 2", () => {
+	expect(timesTwo(10)).toBe(20);
 });
+
+// after doing npm install jest --save-dev
+// have to do npm run test
