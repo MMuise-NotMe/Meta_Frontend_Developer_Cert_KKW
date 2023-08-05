138040e8e7317ad77131a50d0e25cdf80a4aa447
kkw123 <kingwye.kum@my.jcu.edu.au>
2023-08-05 14:46:01 +0800

    Completed solution.js in Array and object iteration assignment

--- Course 2 - Programming with Javascript/Array and object iteration/solution.js
+++ Course 2 - Programming with Javascript/Array and object iteration/solution.js
@@ -1,5 +1,46 @@
+var dairy = [
+	"cheese",
+	"sour cream",
+	"milk",
+	"yogurt",
+	"ice cream",
+	"milkshake",
+];
+
 // Task 1
+function logDairy() {
+	for (let i = 0; i < dairy.length; i++) {
+		console.log(dairy[i]);
+	}
+}
+
+const animal = {
+	canJump: true,
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
 
 // Task 2
+function birdCan() {
+	for (var property of Object.keys(bird)) {
+		console.log(`${property}: ${bird[property]}`);
+	}
+}
 
 // Task 3
+function animalCan() {
+	for (var property of Object.keys(bird)) {
+		console.log(`${property}: ${bird[property]}`);
+	}
+	for (var property of Object.keys(animal)) {
+		console.log(`${property}: ${animal[property]}`);
+	}
+}
+
+logDairy();
+birdCan();
+animalCan();
